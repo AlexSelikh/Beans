3b98863cc4f0c61cc765ee4a01f0e3c9b56e1dda
AlexSelikh <alselikhov@gmail.com>
2023-01-28 15:47:48 +0300

    2.1.4

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -20,9 +20,6 @@ public class AnimalsCage {
         System.out.println("Say:");
         System.out.println(animal.toString());
         System.out.println("At:");
-        System.out.println(new Timer().getTime());
-        System.out.println("________________________");
-        System.out.println("At:");
         System.out.println(timer.getTime());
         System.out.println("________________________");
 
